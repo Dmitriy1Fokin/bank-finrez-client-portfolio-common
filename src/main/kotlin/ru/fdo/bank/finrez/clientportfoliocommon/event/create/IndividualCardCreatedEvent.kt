a1da374a6132564daad46c403c414de0599d7579
Dmitrii Fokin <fokinds1@gmail.com>
2021-01-16 01:14:49 +0300

    added created events

---
+++ src/main/kotlin/ru/fdo/bank/finrez/clientportfoliocommon/event/create/IndividualCardCreatedEvent.kt
@@ -0,0 +1,14 @@
+package ru.fdo.bank.finrez.clientportfoliocommon.event.create
+
+import java.time.LocalDate
+
+data class IndividualCardCreatedEvent(
+    val contractId: String,
+    val clientId: String,
+    val employeeId: String,
+    val programId: String,
+    val contractNum: String,
+    val dateStartContract: LocalDate,
+    val dateEndContract: LocalDate,
+    val dateSigningContract: LocalDate
+)
