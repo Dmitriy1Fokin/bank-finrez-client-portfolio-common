613ba48099b8ba34d18ef60e0ec0529e83ecbd5b
Dmitrii Fokin <fokinds1@gmail.com>
2021-01-24 22:14:33 +0300

    rename data class

--- src/main/kotlin/ru/fdo/bank/finrez/clientportfoliocommon/query/FindAllContractsByParamsQuery.kt
+++ src/main/kotlin/ru/fdo/bank/finrez/clientportfoliocommon/query/FindAllContractsByParamsQuery.kt
@@ -2,7 +2,7 @@ package ru.fdo.bank.finrez.clientportfoliocommon.query
 
 import org.springframework.data.domain.Pageable
 
-data class FindAllContractsByParams(
+data class FindAllContractsByParamsQuery(
     val searchParameters: Map<String, String>,
     val pageable: Pageable
 )
